db97074959e95dd3cd07bfebdf8cf8df414a3933
CartVman <kurtvalderama11@gmail.com>
2022-08-20 19:43:39 +0200

    Commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,10 +7,5 @@ public class HelloWorld {
         for (int i = 1; i <= 10; ++i ) {
             System.out.println(i);
         }
-
-        System.out.println("Petra teller til tretti");
-        for (int i = 11; i <= 30; ++i ) {
-            System.out.println(i);
-        }
     }
 }
